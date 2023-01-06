48481df78598b7d338077a0191d74134402fb9fb
qNnOtTk <84230077+qNnOtTk@users.noreply.github.com>
2023-01-06 22:57:11 +0200

    Delete robloxdata.lua

--- robloxdata.lua
+++
@@ -1,5 +0,0 @@
-http = game:GetService("HttpService")
-message = "wasted"
-print("SENT >> "..message)
-http:PostAsync("http://localhost:1111/test", message,Enum.HttpContentType.ApplicationUrlEncoded)
---print("RECEIVED >> "..http:GetAsync("http://localhost:1111/test"))
